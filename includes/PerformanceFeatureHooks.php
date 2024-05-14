b24f55aac663f25e2ebadfe292d4245280f0b4a0
Evan Mullins <evanrm@gmail.com>
2024-05-14 10:52:20 -0400

    defensive code cleanup

--- includes/PerformanceFeatureHooks.php
+++ includes/PerformanceFeatureHooks.php
@@ -10,43 +10,45 @@ class PerformanceFeatureHooks {
 	 * Constructor.
 	 */
 	public function __construct() {
-        \add_action( 'plugins_loaded', array( $this, 'hooks' ) );
-    }
+		if ( function_exists( 'add_action' ) ) {
+			add_action( 'plugins_loaded', array( $this, 'hooks' ) );
+		}
+	}
 
-    /**
+	/**
 	 * Add hooks.
 	 */
 	public function hooks() {
 
-        // Filter vale based on context
-        \add_filter( 'newfold/features/filter/isEnabled:performance', array( $this, 'filterValue' ) );
+		// Filter vale based on context
+		add_filter( 'newfold/features/filter/isEnabled:performance', array( $this, 'filterValue' ) );
 
-        // Force disable based on context
-        \add_action( 'newfold/features/action/onEnable:performance', array( $this, 'maybeDisable' ) );
+		// Force disable based on context
+		add_action( 'newfold/features/action/onEnable:performance', array( $this, 'maybeDisable' ) );
 
-        // Check if should disable on setup
-        \add_action( 'after_setup_theme', array( $this, 'maybeDisable' ) );
+		// Check if should disable on setup
+		add_action( 'after_setup_theme', array( $this, 'maybeDisable' ) );
 
-    }
+	}
 
-    // Feature filter based on context
-    function filterValue( $value ) {
-        if ( $this->shouldDisable() ) {
-            $value = false;
-        }
-        return $value;
-    }
+	// Feature filter based on context
+	function filterValue( $value ) {
+		if ( $this->shouldDisable() ) {
+			$value = false;
+		}
+		return $value;
+	}
 
-    // Maybe disable
-    function maybeDisable() {
-        if ( $this->shouldDisable() ) {
-            disableFeature('performance');
-        }
-    }
+	// Maybe disable
+	function maybeDisable() {
+		if ( $this->shouldDisable() ) {
+			disableFeature('performance');
+		}
+	}
 
-    // Context condition for disabling feature
-    function shouldDisable() {
-        // check for atomic context
-        return 'atomic' === getContext( 'platform' );
-    }
+	// Context condition for disabling feature
+	function shouldDisable() {
+		// check for atomic context
+		return 'atomic' === getContext( 'platform' );
+	}
 }
