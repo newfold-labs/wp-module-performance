220206732a5280138bf24d1eca30b666c0fbe653
Evan Mullins <evanrm@gmail.com>
2024-05-15 16:29:55 -0400

    move container hooks into hooks class
    also incorporate them to feature enable and disable hooks

--- includes/PerformanceFeature.php
+++ includes/PerformanceFeature.php
@@ -2,15 +2,8 @@
 
 namespace NewfoldLabs\WP\Module\Performance;
 
-use NewfoldLabs\WP\ModuleLoader\Container;
 use NewfoldLabs\WP\Module\Performance\Performance;
-use NewfoldLabs\WP\Module\Performance\CacheTypes\Browser;
-use NewfoldLabs\WP\Module\Performance\CacheTypes\Cloudflare;
-use NewfoldLabs\WP\Module\Performance\CacheTypes\File;
-use NewfoldLabs\WP\Module\Performance\CacheTypes\Skip404;
-use NewfoldLabs\WP\Module\Performance\ResponseHeaderManager;
 
-use function NewfoldLabs\WP\Module\Performance\getCacheLevel;
 use function NewfoldLabs\WP\ModuleLoader\container as getContainer;
 
 /**
@@ -40,41 +33,11 @@ class PerformanceFeature extends \NewfoldLabs\WP\Module\Features\Feature {
 	 */
 	public function initialize() {
 		if ( function_exists( 'add_action' ) ) {
-
 			// Register module
 			add_action(
 				'plugins_loaded',
 				function () {
 					new Performance( getContainer() );
-				}
-			);
-
-			// Container Hooks
-			add_action(
-				'newfold_container_set',
-				function ( Container $container ) {
-					register_activation_hook(
-						$container->plugin()->file,
-						function () use ( $container ) {
-							Skip404::onActivation();
-							File::onActivation();
-							Browser::onActivation();
-							// Add headers to .htaccess
-							$responseHeaderManager = new ResponseHeaderManager();
-							$responseHeaderManager->addHeader( 'X-Newfold-Cache-Level', absint( getCacheLevel() ) );
-						}
-					);
-					register_deactivation_hook(
-						$container->plugin()->file,
-						function () use ( $container ) {
-							Skip404::onDeactivation();
-							File::onDeactivation();
-							Browser::onDeactivation();
-							// Remove all headers from .htaccess
-							$responseHeaderManager = new ResponseHeaderManager();
-							$responseHeaderManager->removeAllHeaders();
-						}
-					);
 				}
 			);
 		}
--- includes/PerformanceFeatureHooks.php
+++ includes/PerformanceFeatureHooks.php
@@ -1,8 +1,18 @@
 <?php
 namespace NewfoldLabs\WP\Module\Performance;
 
+
+use NewfoldLabs\WP\ModuleLoader\Container;
+use NewfoldLabs\WP\Module\Performance\CacheTypes\Browser;
+use NewfoldLabs\WP\Module\Performance\CacheTypes\File;
+use NewfoldLabs\WP\Module\Performance\CacheTypes\Skip404;
+use NewfoldLabs\WP\Module\Performance\ResponseHeaderManager;
+
+use function NewfoldLabs\WP\Module\Performance\getCacheLevel;
+use function NewfoldLabs\WP\ModuleLoader\container as getContainer;
 use function NewfoldLabs\WP\Context\getContext;
 use function NewfoldLabs\WP\Module\Features\disable as disableFeature;
+use function NewfoldLabs\WP\Module\Features\isEnabled;
 
 /**
  * This class adds performance feature hooks.
@@ -15,6 +25,7 @@ class PerformanceFeatureHooks {
 	public function __construct() {
 		if ( function_exists( 'add_action' ) ) {
 			add_action( 'plugins_loaded', array( $this, 'hooks' ) );
+			add_action( 'newfold_container_set', array( $this, 'pluginHooks') );
 		}
 	}
 
@@ -25,7 +36,7 @@ class PerformanceFeatureHooks {
 		// Filter vale based on context
 		add_filter( 'newfold/features/filter/isEnabled:performance', array( $this, 'filterValue' ) );
 		// Force disable based on context
-		add_action( 'newfold/features/action/onEnable:performance', array( $this, 'maybeDisable' ) );
+		add_action( 'newfold/features/action/onEnable:performance', array( $this, 'onFeatureEnable' ) );
 		// Check if should disable on setup
 		add_action( 'after_setup_theme', array( $this, 'maybeDisable' ) );
 	}
@@ -51,6 +62,19 @@ class PerformanceFeatureHooks {
 	public function maybeDisable() {
 		if ( $this->shouldDisable() ) {
 			disableFeature( 'performance' );
+			$this->onDeactivation();
+		}
+	}
+
+	/**
+	 * On enable callback.
+	 *
+	 * @return void
+	 */
+	public function onFeatureEnable() {
+		$this->maybeDisable();
+		if ( isEnabled( 'performance' ) ) {
+			$this->onActivation();
 		}
 	}
 
@@ -62,5 +86,37 @@ class PerformanceFeatureHooks {
 	public function shouldDisable() {
 		// check for atomic context
 		return 'atomic' === getContext( 'platform' );
+	}
+
+	/**
+	 * Hooks for plugin activation/deactivation
+	 */
+	public function pluginHooks( Container $container ) {
+		register_activation_hook( $container->plugin()->file, array( $this, 'onActivation' ) );
+		register_deactivation_hook( $container->plugin()->file, array( $this, 'onDeactivation' ) );
+	}
+
+	/**
+	 * Activation hook to perform when plugin is activated or feature is enabled
+	 */
+	public function onActivation() {
+		Skip404::onActivation();
+		File::onActivation();
+		Browser::onActivation();
+		// Add headers to .htaccess
+		$responseHeaderManager = new ResponseHeaderManager();
+		$responseHeaderManager->addHeader( 'X-Newfold-Cache-Level', absint( getCacheLevel() ) );
+	}
+
+	/**
+	 * Deactivation hook to perform when plugin is deactivated or feature is disabled
+	 */
+	public function onDeactivation() {
+		Skip404::onDeactivation();
+		File::onDeactivation();
+		Browser::onDeactivation();
+		// Remove all headers from .htaccess
+		$responseHeaderManager = new ResponseHeaderManager();
+		$responseHeaderManager->removeAllHeaders();
 	}
 }
