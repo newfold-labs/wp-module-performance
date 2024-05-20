eefe02f2b2415badcb3f422f711e7f779de648fe
Evan Mullins <evanrm@gmail.com>
2024-05-20 15:09:29 -0400

    remove atomic detection hooks
    it has been moved to a standalone module

--- includes/PerformanceFeatureHooks.php
+++ includes/PerformanceFeatureHooks.php
@@ -24,68 +24,8 @@ class PerformanceFeatureHooks {
 	 */
 	public function __construct() {
 		if ( function_exists( 'add_action' ) ) {
-			add_action( 'plugins_loaded', array( $this, 'hooks' ) );
 			add_action( 'newfold_container_set', array( $this, 'pluginHooks') );
 		}
-	}
-
-	/**
-	 * Add hooks.
-	 */
-	public function hooks() {
-		// Filter vale based on context
-		add_filter( 'newfold/features/filter/isEnabled:performance', array( $this, 'filterValue' ) );
-		// Force disable based on context
-		add_action( 'newfold/features/action/onEnable:performance', array( $this, 'onFeatureEnable' ) );
-		// Check if should disable on setup
-		add_action( 'after_setup_theme', array( $this, 'maybeDisable' ) );
-	}
-
-	/**
-	 * Feature filter based on context.
-	 *
-	 * @param boolean $value the value
-	 * @return boolean the filtered value
-	 */
-	public function filterValue( $value ) {
-		if ( $this->shouldDisable() ) {
-			$value = false;
-		}
-		return $value;
-	}
-
-	/**
-	 * Maybe disable the feature.
-	 *
-	 * @return void
-	 */
-	public function maybeDisable() {
-		if ( $this->shouldDisable() ) {
-			disableFeature( 'performance' );
-			$this->onDeactivation();
-		}
-	}
-
-	/**
-	 * On enable callback.
-	 *
-	 * @return void
-	 */
-	public function onFeatureEnable() {
-		$this->maybeDisable();
-		if ( isEnabled( 'performance' ) ) {
-			$this->onActivation();
-		}
-	}
-
-	/**
-	 * Context condition for disabling feature.
-	 *
-	 * @return boolean whether the feature should be disabled
-	 */
-	public function shouldDisable() {
-		// check for atomic context
-		return 'atomic' === getContext( 'platform' );
 	}
 
 	/**
