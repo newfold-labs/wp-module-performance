6eeca8c6f52a09b8e384b7b62c72121c77d72c52
Evan Mullins <evanrm@gmail.com>
2024-05-21 17:20:02 -0400

    lint

--- bootstrap.php
+++ bootstrap.php
@@ -3,12 +3,12 @@
 namespace NewfoldLabs\WP\Module\Performance;
 
 if ( function_exists( 'add_filter' ) ) {
-    add_filter( 
-        'newfold/features/filter/register', 
-        function( $features ) { 
-            return array_merge( $features, array( PerformanceFeature::class ) );
-        }
-    );
+	add_filter(
+		'newfold/features/filter/register',
+		function ( $features ) {
+			return array_merge( $features, array( PerformanceFeature::class ) );
+		}
+	);
 }
 
 new PerformanceFeatureHooks();
