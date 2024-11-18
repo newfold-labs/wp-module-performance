2202638dae6771a9452c2f4c5a669a06f3d04fb9
Armando Liccardo <li.armando82@gmail.com>
2024-11-18 10:35:07 +0000

    added RestApi.php

---
+++ includes/RestApi/RestApi.php
@@ -0,0 +1,36 @@
+<?php 
+namespace NewfoldLabs\WP\Module\Performance\RestApi;
+
+final class RestApi {
+
+	/**
+	 * List of custom REST API controllers
+	 *
+	 * @var array
+	 */
+	protected $controllers = array(
+		'NewfoldLabs\\WP\\Module\\Performance\\RestApi\\LinkPrefetchController',
+	);
+
+	/**
+	 * Setup the custom REST API
+	 */
+	public function __construct() {
+		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+	}
+
+		/**
+	 * Register API routes.
+	 */
+	public function register_routes() {
+		foreach ( $this->controllers as $Controller ) {
+			/**
+			 * Get an instance of the WP_REST_Controller.
+			 *
+			 * @var $instance \WP_REST_Controller
+			 */
+			$instance = new $Controller();
+			$instance->register_routes();
+		}
+	}
+}
