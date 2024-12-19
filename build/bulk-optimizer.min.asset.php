982b8b45fb1cee7b9f3ac4a0a635ac93b3d6a2aa
arunshenoy99 <devarunshenoy99@gmail.com>
2024-12-19 17:23:42 +0530

    Mark Optimized Images

--- build/bulk-optimizer.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'd69f4bfea2649c9caaee');
--- build/lazy-load.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c2241189e2094eeed379');
