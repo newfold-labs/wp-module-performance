a010ca4c219fd96a1ba95025256f836459619bf7
Alessio Torrisi <alessiotorrisidev@gmail.com>
2024-12-30 14:31:01 +0100

    Fix: lint JS files

--- components/clearCache/index.js
+++ components/clearCache/index.js
@@ -1,39 +1,37 @@
-import { Button, Container } from "@newfold/ui-component-library";
+import { Button, Container } from '@newfold/ui-component-library';
 
-const ClearCache = ({ methods, constants }) => {
+const ClearCache = ( { methods, constants } ) => {
+	const clearCache = () => {
+		methods.newfoldPurgeCacheApiFetch(
+			{},
+			methods.setError,
+			( response ) => {
+				methods.makeNotice(
+					'disable-old-posts-comments-notice',
+					constants.text.clearCacheNoticeTitle,
+					null,
+					'success',
+					5000
+				);
+			}
+		);
+	};
 
-    const clearCache = () => {
-        methods.newfoldPurgeCacheApiFetch(
-            {}, 
-            methods.setError, 
-            (response) => {
-                methods.makeNotice(
-                    "disable-old-posts-comments-notice", 
-                    constants.text.clearCacheNoticeTitle,
-                    null,
-                    "success",
-                    5000
-                );
-            }
-        );
-    };
-
-    return (
-        <Container.SettingsField
-            title={constants.text.clearCacheTitle}
-            description={constants.text.clearCacheDescription}
-        >
-            <Button
-                variant="secondary"
-                className="clear-cache-button"
-                disabled={constants.store.cacheLevel > 0 ? false : true}
-                onClick={clearCache}
-                >
-                {constants.text.clearCacheButton}
-            </Button>
-        </Container.SettingsField>
-            
-    );
-;}
+	return (
+		<Container.SettingsField
+			title={ constants.text.clearCacheTitle }
+			description={ constants.text.clearCacheDescription }
+		>
+			<Button
+				variant="secondary"
+				className="clear-cache-button"
+				disabled={ constants.store.cacheLevel > 0 ? false : true }
+				onClick={ clearCache }
+			>
+				{ constants.text.clearCacheButton }
+			</Button>
+		</Container.SettingsField>
+	);
+};
 
 export default ClearCache;
