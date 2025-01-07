64a02550a581112eedaef75414b8f51740153f58
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-01-07 09:34:07 +0100

    Fix: phpcs issue yoda condition

--- includes/burstSafetyModeFunctions.php
+++ includes/burstSafetyModeFunctions.php
@@ -10,7 +10,7 @@ $site_cache_level = get_option( 'newfold_cache_level', 3 );
 $site_skip404     = (bool) get_option( 'newfold_skip_404_handling', true );
 
 if ( defined( 'BURST_SAFETY_MODE' ) && BURST_SAFETY_MODE ) {
-	if ( $newfold_burst_safety_mode === false ) {
+	if ( false === $newfold_burst_safety_mode ) {
 		$current_level = get_option( Performance::OPTION_CACHE_LEVEL );
 		update_option( 'newfold_burst_safety_mode', true );
 		update_option( 'newfold_burst_safety_mode_site_cache_level', $current_level );
