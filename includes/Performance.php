03c49e298b1474543d1a64ed6aa23666ab565294
Alessio Torrisi <alessiotorrisidev@gmail.com>
2025-01-21 15:12:13 +0100

    Fix: php Beautify

--- includes/Performance.php
+++ includes/Performance.php
@@ -368,9 +368,9 @@ class Performance {
 	 *
 	 * @return void
 	 */
-	public function set_default_values_for_jetpack_boost(){
-		$isFreshInstall = container()->get('isFreshInstallation');
-		if( $isFreshInstall && defined( 'JETPACK_BOOST_VERSION' ) ){
+	public function set_default_values_for_jetpack_boost() {
+		$isFreshInstall = container()->get( 'isFreshInstallation' );
+		if ( $isFreshInstall && defined( 'JETPACK_BOOST_VERSION' ) ) {
 			update_option( 'jetpack_boost_status_minify-js', true );
 			update_option( 'jetpack_boost_status_minify-css', true );
 		}
