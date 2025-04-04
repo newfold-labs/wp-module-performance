d684583d6f773f7c22cef28ebd036491b85c1871
arunshenoy99 <devarunshenoy99@gmail.com>
2025-04-04 10:20:33 +0530

    Hide Jetpack Boost Iframe

--- src/sections/JetpackBoost/index.js
+++ src/sections/JetpackBoost/index.js
@@ -138,7 +138,7 @@ const JetpackBoost = () => {
 			iframe = document.createElement( 'iframe' );
 			iframe.src = adminUrl;
 			document.body.appendChild( iframe );
-
+			iframe.style.height = '0';
 			iframe.onload = function () {
 				try {
 					const iframeDocument =
