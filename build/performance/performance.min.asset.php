dec61f8b3835f1069078b276cb789b324218a381
Armando Liccardo <li.armando82@gmail.com>
2025-05-26 11:29:51 +0100

    adding missing js and css files

--- build/performance/performance.min.asset.php
+++ build/performance/performance.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '722a2d93aaeecbf3c936');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '07ccb0a7beb4d9b84039');
