5d9d87ec2ce848384a539b6ea8b2df655d7e84d9
arunshenoy99 <devarunshenoy99@gmail.com>
2025-06-26 21:01:52 +0530

    Update build

--- build/performance/performance.min.asset.php
+++ build/performance/performance.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2d7cba53d04278738f62');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '9e9b09455818ac05187a');
