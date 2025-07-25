e751de209db737d7266e406b58e454c4094b6312
Armando Liccardo <li.armando82@gmail.com>
2025-07-25 09:47:03 +0100

    removed not needed reference and recovered previous version of unit test

--- includes/HealthChecks/PersistentObjectCacheHealthCheck.php
+++ includes/HealthChecks/PersistentObjectCacheHealthCheck.php
@@ -2,7 +2,6 @@
 
 namespace NewfoldLabs\WP\Module\Performance\HealthChecks;
 
-use function NewfoldLabs\WP\Module\LinkTracker\Functions\build_link as buildLink;
 /**
  * Health check for persistent object cache.
  */
