0f1ecfa077f2517ee0036e4f8c7f30a5173ec4f6
Evan Mullins <evanrm@gmail.com>
2025-09-24 14:24:43 -0400

    set up portal rendering in existing app

--- build/performance/performance.min.asset.php
+++ build/performance/performance.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2388d818886b634f8249');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd2637b3154802998f1d1');
