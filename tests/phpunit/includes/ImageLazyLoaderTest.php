b5e3f8579e23c926ca93576ef15f3b45e79095c9
Armando Liccardo <li.armando82@gmail.com>
2025-10-15 12:35:00 +0100

    php lint

--- tests/phpunit/includes/ImageLazyLoaderTest.php
+++ tests/phpunit/includes/ImageLazyLoaderTest.php
@@ -36,7 +36,7 @@ class ImageLazyLoaderTest extends TestCase {
 	public function test_image_lazy_loader() {
 		WP_Mock::expectActionAdded(
 			'wp_enqueue_scripts',
-			[ WP_Mock\Functions::type( ImageLazyLoader::class ), 'enqueue_lazy_loader' ]
+			array( WP_Mock\Functions::type( ImageLazyLoader::class ), 'enqueue_lazy_loader' )
 		);
 
 		$filters_to_mock = array(
@@ -49,18 +49,19 @@ class ImageLazyLoaderTest extends TestCase {
 		foreach ( $filters_to_mock as $filter ) {
 			WP_Mock::expectFilterAdded(
 				$filter,
-				[ WP_Mock\Functions::type( ImageLazyLoader::class ), 'apply_lazy_loading' ]
+				array( WP_Mock\Functions::type( ImageLazyLoader::class ), 'apply_lazy_loading' )
 			);
 		}
 
 		WP_Mock::expectFilterAdded(
 			'render_block',
-			[ WP_Mock\Functions::type( ImageLazyLoader::class ), 'apply_lazy_loading_to_blocks' ],
+			array( WP_Mock\Functions::type( ImageLazyLoader::class ), 'apply_lazy_loading_to_blocks' ),
 			10,
 			2
 		);
 
 		$image_lazy_loader = new ImageLazyLoader();
+
 		$html = '<form>
 		<input type="hidden" value="1">
 		<input name="provider" value="test input" type="hidden">
@@ -73,11 +74,12 @@ class ImageLazyLoaderTest extends TestCase {
 		</script>
 		<h2>Test Title</h2>
 		</form>';
-		$content = $image_lazy_loader->apply_lazy_loading( $html );
-		$this->assertSame($html, $content, 'Content should remain unchanged without images.');
 
-		$html = '<iframe<!-- [et_pb_line_break_holder] -->  src="https://example.com/some_url"<!-- [et_pb_line_break_holder] --> ></iframe>';
 		$content = $image_lazy_loader->apply_lazy_loading( $html );
-		$this->assertSame($html, $content, 'Content should remain unchanged without images.');
+		$this->assertSame( $html, $content, 'Content should remain unchanged without images.' );
+
+		$html    = '<iframe<!-- [et_pb_line_break_holder] -->  src="https://example.com/some_url"<!-- [et_pb_line_break_holder] --> ></iframe>';
+		$content = $image_lazy_loader->apply_lazy_loading( $html );
+		$this->assertSame( $html, $content, 'Content should remain unchanged without images.' );
 	}
 }
